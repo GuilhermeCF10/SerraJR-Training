47df72d0d3e1a43d6cfe3b48c53fca27a79fca85
Guilherme Fialho <guilhermec.fialho@gmail.com>
2021-06-09 20:40:57 -0300

    Introduzindo o React

--- Dia3.1/dia3.1-react/src/index.js
+++ Dia3.1/dia3.1-react/src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+    <link href="https://fonts.googleapis.com/css2?family=Magra&display=swap" rel="stylesheet"/> 
     <App/>
   </React.StrictMode>,
   document.getElementById('root')
