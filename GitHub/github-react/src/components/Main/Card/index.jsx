e0c3f3ae2d1a96b216964efa30e5bc750e497b4f
Guilherme Fialho <guilhermec.fialho@gmail.com>
2021-06-23 11:37:45 -0300

    Alterando a estrutura para funcionar sincronamente

--- GitHub/github-react/src/components/Main/Card/index.jsx
+++ GitHub/github-react/src/components/Main/Card/index.jsx
@@ -10,8 +10,13 @@ export default function Card() {
     const [inputRepository, setInputRepository] = useState("");
     const [membersCards, setMembersCards] = useState([]);
 
-    const [_userInfo, setUserInfo] = useState();
-    const [_repoInfo, setRepoInfo] = useState();
+    const [userInfo, setUserInfo] = useState();
+    const [repoInfo, setRepoInfo] = useState();
+
+
+    
+    
+        
 
 
     function writeInputName(e) {
@@ -22,42 +27,7 @@ export default function Card() {
         setInputRepository(e.target.value);
     }
 
-    function getProfileInfo(url) {
-        axios.get(url) .then( (res) => {
-            const data = {
-                avatar: res.data.avatar_url,
-                name: res.data.name,
-                bio: res.data.bio,
-                location: res.data.location,
-                followers: res.data.followers,
-                following: res.data.following,
-            }
-            
-            setUserInfo(data)
-        }) .catch ( (error) => {console.log(error)} )
-    }
-
-    function getProfileRepositoryInfo(url) {
-        axios.get(url) .then( (res) => {
-            const listRepos = res.data;
-
-            for (let eachRepo of listRepos) {
-                let currentRepo = eachRepo.name.toLowerCase()
-                let mainRepo = inputRepository.toLowerCase()
-
-                if (currentRepo === mainRepo) {
-                    const data = {
-                        nameRepo: eachRepo.name,
-                        createAtRepo: eachRepo.created_at,
-                        urlRepo: eachRepo.html_url,
-                    }
-                    setRepoInfo(data)
-                }
-            }
-        }) .catch ( (error) => {console.log(error)} )
-    }
-
-    function searchOwner() {
+    async function searchOwner() {
 
         if (!inputName || !inputRepository) {
             return alert("Preencha os campos necessários!")
@@ -68,46 +38,108 @@ export default function Card() {
         const urlRepository = `https://api.github.com/users/${inputName}/repos`
 
         
-          
-        getProfileInfo(urlProfile)
+        // Response of Axios Get Method (profile)
+        const responseProfile = await axios.get(urlProfile)
 
-        getProfileRepositoryInfo(urlRepository)
-    
-        
-        if (!_userInfo || !_repoInfo) {
-            return;
-        } else {
-
-            setMembersCards( (prevState) => {
-
-                const newPrevState = prevState.filter( (eachPrev) => {
-                    alert("Você não pode adicionar dois repositórios iguais!")
-                    return eachPrev.repoName !== _repoInfo.nameRepo
-                })
-    
-                if (newPrevState.length === prevState.length) {
-                    return [...prevState, {
-                        key: Math.random(), 
-                        avatar: _userInfo.avatar, name: _userInfo.name, location: _userInfo.location, bio: _userInfo.bio, following: _userInfo.following, followers: _userInfo.followers, 
-                        repoName: _repoInfo.nameRepo ,repoDate: _repoInfo.createAtRepo.slice(0, 10), repoUrl: _repoInfo.urlRepo,
-                    }];
-                } else {
-                    return [...newPrevState, {
-                        key: Math.random(), 
-                        avatar: _userInfo.avatar, name: _userInfo.name, location: _userInfo.location, bio: _userInfo.bio, following: _userInfo.following, followers: _userInfo.followers, 
-                        repoName: _repoInfo.nameRepo ,repoDate: _repoInfo.createAtRepo.slice(0, 10), repoUrl: _repoInfo.urlRepo,
-        
-                    }];
-                }
-            })
-            
-            setInputName("");
-            setInputRepository("");
-            setUserInfo();
-            setRepoInfo();
+        const dataProfile = {
+            avatar: responseProfile.data.avatar_url,
+            name: responseProfile.data.name,
+            bio: responseProfile.data.bio,
+            location: responseProfile.data.location,
+            followers: responseProfile.data.followers,
+            following: responseProfile.data.following,
         }
-    }
         
+        setUserInfo( () => {
+            return dataProfile;
+        })
+
+        // Response of Axios Get Method (repository)
+        
+        const responseRepository = await axios.get(urlRepository)
+
+        const listRepos = responseRepository.data;
+
+        for (let eachRepo of listRepos) {
+            let currentRepo = eachRepo.name.toLowerCase()
+            let mainRepo = inputRepository.toLowerCase()
+
+            if (currentRepo === mainRepo) {
+                
+                const repositoryData = {
+                    nameRepo: eachRepo.name,
+                    createAtRepo: eachRepo.created_at,
+                    urlRepo: eachRepo.html_url,
+                }
+                setRepoInfo( () => {
+                    return repositoryData;
+                })
+            }
+        }
+        
+        
+
+        // if (!userInfo || !repoInfo) {
+        //     return searchOwner()
+        // }
+        
+        console.log(userInfo)
+        console.log(repoInfo)
+        addMember();
+
+        
+        
+        
+            // setMembersCards( (prevState) => {
+
+            //     const newPrevState = prevState.filter( (eachPrev) => {
+            //         alert("Você não pode adicionar dois repositórios iguais!")
+            //         return eachPrev.repoName !== _repoInfo.nameRepo
+            //     })
+    
+            //     if (newPrevState.length === prevState.length) {
+            //         return [...prevState, {
+            //             key: Math.random(), 
+            //             avatar: _userInfo.avatar, name: _userInfo.name, location: _userInfo.location, bio: _userInfo.bio, following: _userInfo.following, followers: _userInfo.followers, 
+            //             repoName: _repoInfo.nameRepo ,repoDate: _repoInfo.createAtRepo.slice(0, 10), repoUrl: _repoInfo.urlRepo,
+            //         }];
+            //     } else {
+            //         return [...newPrevState, {
+            //             key: Math.random(), 
+            //             avatar: _userInfo.avatar, name: _userInfo.name, location: _userInfo.location, bio: _userInfo.bio, following: _userInfo.following, followers: _userInfo.followers, 
+            //             repoName: _repoInfo.nameRepo ,repoDate: _repoInfo.createAtRepo.slice(0, 10), repoUrl: _repoInfo.urlRepo,
+        
+            //         }];
+            //     }
+            // })
+    }
+
+    function addMember() {
+        const newMemberData = {
+            key: Math.random(), 
+            avatar: userInfo.avatar, name: userInfo.name, location: userInfo.location, bio: userInfo.bio, following: userInfo.following, followers: userInfo.followers, 
+            repoName: repoInfo.nameRepo ,repoDate: repoInfo.createAtRepo.slice(0, 10)
+            , repoUrl: repoInfo.urlRepo,
+        }
+
+        setMembersCards( (prevState) => {
+            return [...prevState, newMemberData]
+        })
+
+        setInputName( () => {
+            return "";
+        });
+        setInputRepository( () => {
+            return "";
+        });
+        setUserInfo( () => {
+            return;
+        });
+        setRepoInfo( () => {
+            return;
+        });
+    }
+
     function renderProfiles() {
         return membersCards.map( (currentProfile) => {
             return <Item name={currentProfile.name} avatar={currentProfile.avatar} location={currentProfile.location} bio={currentProfile.bio} following={currentProfile.following} followers={currentProfile.followers}
